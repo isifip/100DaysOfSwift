cf912f6888823634a0af3e0e3f86017aa8bf0c67
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-03 16:17:19 +0400

    + Sharing things using UIActivityViewController. This is part of Project 3

--- Project1-StormViewer/Project1-StormViewer/Controllers/DetailViewController.swift
+++ Project1-StormViewer/Project1-StormViewer/Controllers/DetailViewController.swift
@@ -19,6 +19,9 @@ class DetailViewController: UIViewController {
         
         title = detailImageTitle
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         // Do any additional setup after loading the view.
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -34,6 +37,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 }
