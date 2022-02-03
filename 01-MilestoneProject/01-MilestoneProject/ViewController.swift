d8ecd4b3b6fd726b59a805d195a1e2ffeee8c085
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-03 18:14:43 +0400

    + First milestone project.

---
+++ 01-MilestoneProject/01-MilestoneProject/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  01-MilestoneProject
+//
+//  Created by Irakli Sokhaneishvili on 03.02.22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.contains("png") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
+        return cell
+    }
+}
+
