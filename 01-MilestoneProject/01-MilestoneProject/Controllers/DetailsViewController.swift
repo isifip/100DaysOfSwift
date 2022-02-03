16065f0583701c0981c5a5f653465c0b5f427b18
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-03 19:43:55 +0400

    + Share option

--- 01-MilestoneProject/01-MilestoneProject/Controllers/DetailsViewController.swift
+++ 01-MilestoneProject/01-MilestoneProject/Controllers/DetailsViewController.swift
@@ -18,11 +18,22 @@ class DetailsViewController: UIViewController {
         
         navigationItem.largeTitleDisplayMode = .never
         title = selectedImage
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
     }
     
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1) else {
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
     
 }
