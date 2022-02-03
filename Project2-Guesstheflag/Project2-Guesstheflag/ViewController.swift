e3abc96463199eb7144278635d5bbdbefa0f9c66
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-03 15:40:33 +0400

    + Project2 initial commit with flag pictures

---
+++ Project2-Guesstheflag/Project2-Guesstheflag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2-Guesstheflag
+//
+//  Created by Irakli Sokhaneishvili on 03.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
