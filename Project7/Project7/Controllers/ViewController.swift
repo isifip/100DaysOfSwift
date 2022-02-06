8f7ea8e0059b68dd9dfecfbae55bdfaa29f6902e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-06 20:03:01 +0400

    + Project 7 challenges.

--- Project7/Project7/Controllers/ViewController.swift
+++ Project7/Project7/Controllers/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    
+    var filterKeyword: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Petitions"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credit", style: .plain, target: self, action: #selector(creditTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+        
         
         let urlString: String
         
@@ -33,6 +43,46 @@ class ViewController: UITableViewController {
         showError()
     }
     
+    @objc func creditTapped() {
+        let ac = UIAlertController(title: "Credit", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    
+    @objc func filterTapped() {
+        let ac = UIAlertController(title: "Filter by", message: "Filter the data using the keyword of your choice", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        let action = UIAlertAction(title: "Search", style: .default){ [weak self, weak ac] _ in
+            self?.filterKeyword = ac?.textFields?[0].text ?? ""
+            self?.filterData()
+            self?.tableView.reloadData()
+        }
+        ac.addAction(action)
+        present(ac, animated: true)
+    }
+    
+    func filterData() {
+        if filterKeyword.isEmpty {
+            filteredPetitions = petitions
+            navigationItem.leftBarButtonItem?.title = "Filter"
+            return
+        }
+        navigationItem.leftBarButtonItem?.title = "(keyword: \(filterKeyword))"
+        
+        filteredPetitions = petitions.filter() { petition in
+            if let _ = petition.title.range(of: filterKeyword, options: .caseInsensitive) {
+                return true
+            }
+            if let _ = petition.body.range(of: filterKeyword, options: .caseInsensitive) {
+                return true
+            }
+            return false
+        }
+        
+    }
+    
     func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -43,17 +93,18 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filterData()
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -61,7 +112,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 
