c70b52ff7b263f5ffcabf414230f3f8ecf13ac1b
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-06 17:20:09 +0400

    + Parsing JSON using the Codable protocol

---
+++ Project7/Project7/Models/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Irakli Sokhaneishvili on 06.02.22.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Models/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Irakli Sokhaneishvili on 06.02.22.
+//
+
+import Foundation
+
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
