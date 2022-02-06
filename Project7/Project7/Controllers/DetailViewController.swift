c21303d5d472fc6f420288cbccdd7f5ff270c7fe
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-06 18:17:17 +0400

    + Rendering a petition: loadHTMLString

---
+++ Project7/Project7/Controllers/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Irakli Sokhaneishvili on 06.02.22.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let detailItem = detailItem else { return }
+
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 110%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+
+}
