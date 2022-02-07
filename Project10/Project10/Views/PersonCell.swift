17d8ffc5f83494c2ebfe58e9a925f7ed8fd4c645
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-07 17:53:53 +0400

    + UiCollectionView cell

---
+++ Project10/Project10/Views/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Irakli Sokhaneishvili on 07.02.22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
