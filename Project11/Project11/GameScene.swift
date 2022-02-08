7b243e2bac75ce7ad40a350bafec0e613524093c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-08 12:24:34 +0400

    + Customise Edit mode, add random rectangles.

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -65,13 +65,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        ball.physicsBody?.restitution = 0.4
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-        ball.position = location
-        ball.name = "ball"
-        addChild(ball)
+        let objects = nodes(at: location)
+        if objects.contains(editLabel) {
+            editingMode.toggle()
+        } else {
+            if editingMode {
+                // ccreate a box
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(
+                    color: UIColor(red: CGFloat.random(in: 0...1),
+                                   green: CGFloat.random(in: 0...1),
+                                   blue: CGFloat.random(in: 0...1),
+                                   alpha: 1),
+                    size: size)
+                
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+                
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+                addChild(box)
+                
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                ball.physicsBody?.restitution = 0.4
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                ball.position = location
+                ball.name = "ball"
+                addChild(ball)
+            }
+        }
     }
     
     func makeBouncer(at position: CGPoint) {
