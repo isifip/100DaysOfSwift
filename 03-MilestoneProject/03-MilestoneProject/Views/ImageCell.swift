0d7816b19921a402933279a2c3df6c4c234d64b7
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-09 15:09:06 +0400

    +

---
+++ 03-MilestoneProject/03-MilestoneProject/Views/ImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCell.swift
+//  03-MilestoneProject
+//
+//  Created by Irakli Sokhaneishvili on 09.02.22.
+//
+
+import Foundation
+import UIKit
+
+
+class ImageCell: UITableViewCell {
+    @IBOutlet var thumbnailView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
