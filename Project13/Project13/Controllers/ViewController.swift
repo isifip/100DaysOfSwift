27c67a91a19df7a31b0d251828f69a3d2ce5758d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-09 19:14:52 +0400

    + interface

--- Project13/Project13/Controllers/ViewController.swift
+++ Project13/Project13/Controllers/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    @IBAction func save(_ sender: UIButton) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
 
