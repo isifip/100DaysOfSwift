7a460eab0e85cf82afec528687a2afbeeda44940
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-11 14:45:32 +0400

    + Initial commit

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Irakli Sokhaneishvili on 11.02.22.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
