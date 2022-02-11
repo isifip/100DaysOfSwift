66245bb99a6390fd97d3d64f43737e56c3341b5c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-11 15:06:00 +0400

    + Background and layout

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -10,14 +10,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
+        
     }
 }
