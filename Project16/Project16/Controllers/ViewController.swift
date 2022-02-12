f377d3d779637e50095c804239a5deead7b31310
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-12 15:38:06 +0400

    + Displaying some capital annotations on map view

--- Project16/Project16/Controllers/ViewController.swift
+++ Project16/Project16/Controllers/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home of the 2012 Summer Olympics")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Lights")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        
+        mapView.addAnnotations([london, oslo, paris, rome])
+       
     }
 
 
