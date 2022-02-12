17bb328846399111a48b2f1cd143b4591c83a356
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-02-12 15:20:00 +0400

    + Project 16, initial commit

---
+++ Project16/Project16/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Irakli Sokhaneishvili on 12.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
