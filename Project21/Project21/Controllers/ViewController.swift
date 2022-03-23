0a16b3caee45389758a7b1aa1081382e17f2eae2
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-23 15:08:52 +0400

    + Schedule the notification

--- Project21/Project21/Controllers/ViewController.swift
+++ Project21/Project21/Controllers/ViewController.swift
@@ -30,7 +30,24 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
         
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+        //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 
 }
