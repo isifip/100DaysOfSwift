0703d250617384514021ac863eaf6c7e40641b9c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-23 15:00:55 +0400

    + Navigation bar buttons + UserNotificationCenter for authorization

--- Project21/Project21/Controllers/ViewController.swift
+++ Project21/Project21/Controllers/ViewController.swift
@@ -5,15 +5,33 @@
 //  Created by Irakli Sokhaneishvili on 23.03.22.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
-
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yes")
+            } else {
+                print("No")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal() {
+        
+    }
 
 }
 
