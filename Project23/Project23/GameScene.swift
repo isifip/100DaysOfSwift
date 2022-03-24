4f082edfe0856ee88e1b3b81d44806d006721a86
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-24 17:39:56 +0400

    + Swoosh sound when slicing

--- Project23/Project23/GameScene.swift
+++ Project23/Project23/GameScene.swift
@@ -24,6 +24,7 @@ class GameScene: SKScene {
     var activeSliceFG: SKShapeNode!
     
     var activeSlicePoints = [CGPoint]()
+    var isSwooshSoundActive = false
     
     
     override func didMove(to view: SKView) {
@@ -83,6 +84,23 @@ class GameScene: SKScene {
         let location = touch.location(in: self)
         activeSlicePoints.append(location)
         redrawActiveSlice()
+        
+        if !isSwooshSoundActive {
+            playSwooshSound()
+        }
+    }
+    
+    func playSwooshSound() {
+        isSwooshSoundActive = true
+        
+        let randomNumber = Int.random(in: 1...3)
+        let soundName = "swoosh\(randomNumber).caf"
+        
+        let swooshSound = SKAction.playSoundFileNamed(soundName, waitForCompletion: true)
+        
+        run(swooshSound) { [weak self] in
+            self?.isSwooshSoundActive = false
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
