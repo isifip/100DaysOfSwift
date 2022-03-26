f3db8ad3c5c9d8f9cac03ec0c67f44997164c453
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-26 19:17:45 +0400

    + Create Sandbox

---
+++ Project27/Project27/Controllers/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewController.swift
+//  Project27
+//
+//  Created by Irakli Sokhaneishvili on 26.03.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        drawRectangle()
+    }
+
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
+    func drawRectangle() {
+        
+    }
+    
+}
