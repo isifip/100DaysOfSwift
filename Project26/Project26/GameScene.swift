e790ad5b6a0efecdb8a9fcf164c20a475ddf10e7
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-26 16:37:57 +0400

    + Simulate tilting. using touches

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -18,6 +18,7 @@ enum CollisinTypes: UInt32 {
 class GameScene: SKScene {
     
     var player: SKSpriteNode!
+    var lastTouchPosition: CGPoint?
   
     override func didMove(to view: SKView) {
         
@@ -29,6 +30,8 @@ class GameScene: SKScene {
         
         loadLevel()
         createPlayer()
+        
+        physicsWorld.gravity = .zero
     }
     
     func loadLevel() {
@@ -125,5 +128,22 @@ class GameScene: SKScene {
         player.physicsBody?.collisionBitMask = CollisinTypes.wall.rawValue
         
         addChild(player)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        lastTouchPosition = location
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastTouchPosition = nil
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let lastTouchPosition = lastTouchPosition {
+            let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
     }
 }
