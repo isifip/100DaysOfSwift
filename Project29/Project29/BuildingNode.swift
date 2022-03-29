ce58113521c6aa1084207aa583a35d98972d78bf
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-03-29 15:41:40 +0400

    + Building the environment pt2

--- Project29/Project29/BuildingNode.swift
+++ Project29/Project29/BuildingNode.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class BuildingNode: SKSpriteNode {
     var currentImage: UIImage!
-    
+
     func setup() {
         name = "building"
-        
+
         currentImage = drawBuilding(size: size)
         texture = SKTexture(image: currentImage)
-        
+
         configurePhysics()
     }
-    
+
     func configurePhysics() {
         physicsBody = SKPhysicsBody(texture: texture!, size: size)
         physicsBody?.isDynamic = false
